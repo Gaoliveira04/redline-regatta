e3a47fb9a9ad767a48e4b3bc935b320d9b07edfb
Gaoliveira04 <gaoliveira04@gmail.com>
2026-02-17 18:21:36 +0000

    Added KeyboardInterrupt during game (only works on the venue view, not during choices)

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import sys
 from src.controllers.menu import MenuController
 from src.controllers.game import GameController
 
 def main():
-    while True:
-        signal, chosen_colors = MenuController.run_menu()
+    try:
+        while True:
+            signal, chosen_colors = MenuController.run_menu()
 
-        if signal == "Start":
-            game = GameController(chosen_colors)
-            game.run_game()
-        elif signal == "Exit":
-            print("\nThanks for playing! See you at the next starting line.")
-            break
+            if signal == "Start":
+                game = GameController(chosen_colors)
+                game.run_game()
+            elif signal == "Exit":
+                print("\nThanks for playing! See you at the next starting line.")
+                break
+
+        sys.exit(0)
+    except KeyboardInterrupt:
+        print("\n[!] Game closed manually. See you on the water!")
+        sys.exit(0)
 
 if __name__ == "__main__":
     main()
